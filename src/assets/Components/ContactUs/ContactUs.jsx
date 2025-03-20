91b5b72c5773807f133cc8eb48778d6fd379c887
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2025-03-21 01:23:30 +0530

    "img bug 1 solution"

--- src/assets/Components/ContactUs/ContactUs.jsx
+++ src/assets/Components/ContactUs/ContactUs.jsx
@@ -85,7 +85,7 @@ const ContactUs = () => {
                             </div>
                                 <div className='flex gap-2 mt-10 justify-end border-t border-[#dededf] pt-10'>
                                     <div className='p-4 bg-white rounded-lg inline-block border border-[#2f00fefe]'>
-                                        <img className='' src="public/img/instagram.png" alt="" />
+                                        <img className='' src="img/instagram.png" alt="" />
                                     </div>
                                     <div className='p-4 bg-white rounded-lg inline-block border border-[#2f00fefe]'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" color="#2f00ff" fill="blue">
